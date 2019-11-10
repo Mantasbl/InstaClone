5cddb003bf57b7e44233b8e07791c4a7cda676ff
Mantasbl <mantas.baltakis@gmail.com>
2019-11-10 10:14:58 +0000

    Profile create/edit, posts create

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,17 +4,22 @@
 <div class="container">
    <div class="row">
        <div class="col-3 p-5">
-           <img src="https://scontent-lhr3-1.cdninstagram.com/vp/f8daa557de934c0a7a3eba8779c30774/5E61C384/t51.2885-19/s320x320/61402573_2308681072677106_8323839489449394176_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com" alt="" 
+           <img src="/storage/{{ $user->profile->image }}" alt="" 
                 class="rounded-circle"
                 height="150px">
        </div>
        <div class="col-9 pt-5">
            <div class="d-flex justify-content-between align-items-baseline">
                <h1>{{$user->username}}</h1>
-                <a href="#">Add new post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
             </div>
+            @can('update', $user->profile) 
+                <a href="/profile/{{$user->id}}/edit">Edit Profile name</a>
+            @endcan
            <div class="d-flex">
-               <div class="pr-4"><strong>153</strong> posts</div>
+               <div class="pr-4"><strong>{{$user->posts->count()}}</strong> posts</div>
                <div class="pr-4"><strong>23k</strong> follower</div>
                <div class="pr-4"><strong>603</strong> following</div>
            </div>
@@ -24,15 +29,11 @@
         </div>
    </div>
    <div class="row pt-5">
-       <div class="col-4">
-           <img src="/assets/posts/Portfolio.png" alt="" class="w-100">
+       @foreach($user->posts as $post)
+       <div class="col-4 pb-4">
+           <a href="/p/{{$post->id}}"><img src="/storage/{{$post->image }}" alt="" class="w-100"></a>
        </div>
-       <div class="col-4">
-           <img src="/assets/posts/MyPuppyDen.png" alt="" class="w-100">
-       </div>
-       <div class="col-4">
-           <img src="/assets/posts/Todo.png" alt="" class="w-100">
-       </div>
-   </div>
+        @endforeach
+    </div>
 </div>
 @endsection
